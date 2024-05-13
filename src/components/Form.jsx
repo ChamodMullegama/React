d6efb2c8b297af466a3cd1ebae93a0ac675f9ff6
ChamodMullegama <chamod.mullegama@gmail.com>
2024-05-13 16:01:11 +0530

    maliple inpus

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,11 +1,21 @@
 import { useState } from "react"
 
 export default function Form(){
-    const [name, setname] = useState("");
+    const [name, setname] = useState({fname:"",lname:""});
     return(
         <div>
+            {name.fname} - {name.lname}
             <form>
-                <input onChange={(e)=>setname(e.target.value)} type="text" value={name}/>
+                <input 
+                onChange={(e)=>setname({...name,fname:e.target.value})} 
+                type="text" 
+                value={name.fname}
+                />
+                <input 
+                onChange={(e)=>setname({...name,lname:e.target.value})} 
+                type="text" 
+                value={name.lname}
+                />
             </form>
         </div>
     )
