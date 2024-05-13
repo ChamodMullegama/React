f0301b34b4db1395e2b9c1be978065e3b6a81cb3
ChamodMullegama <chamod.mullegama@gmail.com>
2024-05-13 15:08:53 +0530

    modify use stats

--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -2,11 +2,20 @@ import { useState } from "react"
 
 export default function Counter(){
    const [count,setcount] = useState(0);
+   const [incremantby,sincremanthbycount] = useState(1)
+
    function handelonclick(){
-      setcount(count+1)
+      setcount(count+incremantby)
    }
    function handelonclickdis(){
-      setcount(count-1)
+      setcount(count-incremantby)
+   }
+
+   function handelonclickinc(){
+      sincremanthbycount(incremantby+1)
+   }
+   function handelonclickdes(){
+      sincremanthbycount(incremantby-1)
    }
     return(
 
@@ -14,6 +23,10 @@ export default function Counter(){
             <h1>Number is : {count}</h1>
             <button onClick={handelonclick}>incress</button>
             <button onClick={handelonclickdis}>dicress</button>
+
+            <h1>Number is : {incremantby}</h1>
+            <button onClick={handelonclickinc}>increment</button>
+            <button onClick={handelonclickdes}>descriment</button>
         </div>
     )
 }
