88fec57400e2be1b259de66a18b0bf4c7b1c7ee1
ChamodMullegama <chamod.mullegama@gmail.com>
2024-05-16 16:46:26 +0530

    add style input feld and nav bar

--- src/components/Find.jsx
+++ src/components/Find.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react';
+import styles from './Find.module.css'; // Import CSS module
 
 const URL = "https://api.spoonacular.com/recipes/complexSearch";
 const API_KEY = "dee50e7439ca4117b50468f761e1ea9e";
@@ -17,8 +18,9 @@ function Find({ setfooddata }) {
     }, [query, setfooddata]);
 
     return (
-        <div>
+        <div className={styles.findContainer}>
             <input 
+                className={styles.searchinput} // Apply CSS module class
                 type="text"
                 value={query}
                 onChange={(e) => setquery(e.target.value)}
