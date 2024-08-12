aa63ef68c1faa36bf18e4a9ea38dc800953bb1c0
Chamod Mullegama <149089781+ChamodMullegama@users.noreply.github.com>
2024-08-12 22:50:34 +0530

    Update Navbar.jsx

--- src/Navbar.jsx
+++ src/Navbar.jsx
@@ -3,27 +3,13 @@ export default function Navbar(){
     return(
         <div>
             <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
+           
 
 
 
 
                    <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
-               <h1 className={styles.nav}>Navbar</h1>
+             
         </div>
     )
 }
